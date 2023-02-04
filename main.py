a932de993b64aed4da55aaac83b48775f6a88593
TabarakoAkula <mishabor7@mail.ru>
2023-02-04 19:35:33 +0300

    Update keyboard

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import sys
+from PyQt5 import QtWidgets
+from window import Ui_MainWindow
+
 longitude, width = input('Введите долготу и широту: ').split()
+
+
+def load_window():
+    app = QtWidgets.QApplication(sys.argv)
+    widget = QtWidgets.QWidget()
+    ui = Ui_MainWindow()
+    ui.setupUi(widget)
+    widget.show()
+    sys.exit(app.exec_())
+
+
+load_window()
+
--- window.py
+++ window.py
@@ -1,10 +1,13 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QFileDialog, QLabel
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QLabel
 from PyQt5.QtGui import QPixmap
 from PIL import Image
+import keyboard
 
 
 class Ui_MainWindow(object):
+    scale = 1
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(800, 600)
@@ -17,16 +20,22 @@ class Ui_MainWindow(object):
         self.image.resize(650, 450)
         self.pixmap = QPixmap(self.fname)
         self.image.setPixmap(self.pixmap)
-        
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        keyboard.on_press_key("UP", lambda _: self.change_value("up"))
+        keyboard.on_press_key("DOWN", lambda _: self.change_value("down"))
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MapViewer"))
 
+    def change_value(self, name):
+        self.scale = name
+        #  if ... : ...
+        #  изменение запроса карты
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
