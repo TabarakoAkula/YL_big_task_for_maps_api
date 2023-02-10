6a05f9086bcf1c697fdb1d8eab27eac3f22efa8d
Evgeny <evgeny.dudko666@gmail.com>
2023-02-10 21:29:03 +0300

    task 3

--- window.py
+++ window.py
@@ -6,6 +6,7 @@ import requests
 
 class Ui_MainWindow(object):
     scale = 1
+    size = 1
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
@@ -19,12 +20,16 @@ class Ui_MainWindow(object):
             self.size = 17
         if self.size < 1:
             self.size = 1
-        self.get(self.size)
+        self.get()
         self.label.move(75, 75)
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
         keyboard.on_press_key("PgUp", lambda _: self.change_value("up"))
         keyboard.on_press_key("PgDown", lambda _: self.change_value("down"))
+        keyboard.on_press_key("UP", lambda _: self.change_coordinates("Up"))
+        keyboard.on_press_key("DOWN", lambda _: self.change_coordinates("Down"))
+        keyboard.on_press_key("LEFT", lambda _: self.change_coordinates("Left"))
+        keyboard.on_press_key("RIGHT", lambda _: self.change_coordinates("Right"))
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
@@ -40,10 +45,29 @@ class Ui_MainWindow(object):
             self.size = 17
         if self.size < 1:
             self.size = 1
-        self.get(self.size)
+        self.get()
 
-    def get(self, size):
-        link = f'https://static-maps.yandex.ru/1.x/?ll={self.longitude},{self.latitude}&size=650,450&l=map&z={size}'
+    def change_coordinates(self, name):
+        self.cor = name
+        self.del_longitude = 180 / self.size / 5
+        self.del_latitude = 360 / self.size / 5
+        if name == 'Right':
+            if float(self.longitude) + self.del_longitude <= 180:
+                self.longitude = str(float(self.longitude) + self.del_longitude)
+        if name == 'Left':
+            if float(self.longitude) - self.del_longitude >= -180:
+                self.longitude = str(float(self.longitude) - self.del_longitude)
+        if name == 'Down':
+            if float(self.latitude) - self.del_latitude >= -90:
+                self.latitude = str(float(self.latitude) - self.del_latitude)
+        if name == 'Up':
+            if float(self.latitude) + self.del_latitude <= 90:
+                self.latitude = str(float(self.latitude) + self.del_latitude)
+        self.get()
+
+
+    def get(self):
+        link = f'https://static-maps.yandex.ru/1.x/?ll={self.longitude},{self.latitude}&size=650,450&l=map&z={self.size}'
         response = requests.get(link)
         with open('map.png', 'wb') as file:
             file.write(response.content)
